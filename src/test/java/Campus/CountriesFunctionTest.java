5c460e8bdf5680559e43e344cc7815a924494181
fundakhan <fundaakhan@gmail.com>
2022-10-18 22:20:38 -0400

    Rest Assured TestNG Jenkins icin delete hatali duruma getirildi

--- src/test/java/Campus/CountriesFunctionTest.java
+++ src/test/java/Campus/CountriesFunctionTest.java
@@ -109,7 +109,7 @@ public class CountriesFunctionTest {
                 .delete("/school-service/api/countries/" + country_id)
                 .then()
                 .log().body()
-                .statusCode(200);
+                .statusCode(208);
 
     }
 
